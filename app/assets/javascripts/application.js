3b294edd906867be0076a1ff227841b6fa7c0f1b
TavernGoal <windack@qq.com>
2014-06-27 16:56:39 +0800

    a commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 // require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+// require twitter/bootstrap
 //= require wice_grid
 //= require turbolinks
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module LemonBaseSystemmanager
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*').to_s]
 
-    platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
-    Bundler.require(platform)
+    # platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
+    # Bundler.require(platform)
   end
 end
