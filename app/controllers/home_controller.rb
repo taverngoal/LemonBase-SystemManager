a4c532e8cc73763e27fbfae3482cec0b408b2220
TavernGoal <windack@qq.com>
2014-07-05 19:35:15 +0800

    a commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   layout 'application'
+
   def index
   end
 
@@ -8,5 +9,22 @@ class HomeController < ApplicationController
   end
 
   def login
+  end
+
+  def register
+
+  end
+
+  def sign_up
+    @user = User.new(params.require(:user).permit!)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to home_login_path, notice: 'Registration was successfully .' }
+      else
+        flash[:error] = @user.errors.as_json
+        p @user.errors
+        format.html { render :register }
+      end
+    end
   end
 end
