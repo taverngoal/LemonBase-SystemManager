79c7dec55fb519339e8bfab6cd70df82d72f9937
TavernGoal <windack@qq.com>
2014-07-08 20:02:27 +0800

    a commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }
+  validates :password, length: {minimum: 3}
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
   validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :email, format: {with: /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/, :multiline => true, message: '邮件格式不对'}
 end
