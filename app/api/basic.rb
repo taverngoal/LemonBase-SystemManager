557e327e2cfc72ceacde7fe7f879d8f77c77f872
TavernGoal <windack@qq.com>
2014-07-08 20:42:15 +0800

    a commit

--- app/api/basic.rb
+++ app/api/basic.rb
@@ -2,9 +2,9 @@ class BasicAPI < Grape::API
   format :json
 
   resource :users do
-    # get do
-    #   User.all().select(:email, :name, :nick, :birth, :address, :phone)
-    # end
+    get do
+      User.all().select(:email, :name, :nick, :birth, :address, :phone)
+    end
 
 
     params do
@@ -16,7 +16,7 @@ class BasicAPI < Grape::API
       optional :address, type: String
       optional :phone, type: String
     end
-    get do
+    post do
       user = User.new email: params[:email], password: params[:password], password_confirmation: params[:password],
                       name: params[:name], nick: params[:nick], birth: params[:birth],
                       address: params[:address], phone: params[:phone]
@@ -26,5 +26,38 @@ class BasicAPI < Grape::API
         user.errors
       end
     end
+
+    params do
+      requires :id, type: Integer
+      requires :email, type: String, regexp: /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
+      optional :password, type: String
+      optional :name, type: String
+      optional :nick, type: String
+      optional :birth, type: Date
+      optional :address, type: String
+      optional :phone, type: String
+    end
+    put do
+      user = User.find params[:id]
+      if user
+        user.update_attributes email: params[:email], password: params[:password], password_confirmation: params[:password],
+                               name: params[:name], nick: params[:nick], birth: params[:birth],
+                               address: params[:address], phone: params[:phone]
+      end
+    end
+
+    params do
+      requires :id, type: Integer
+    end
+    delete do
+      user = User.find params[:id]
+      if user.destroy
+        true
+      else
+        user.errors
+      end
+    end
+
+
   end
 end
